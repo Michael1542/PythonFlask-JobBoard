115fa25bc74492e6668471a9e9cd3bebf98b0caf
Michael Wang <michael.yj.wang@gmail.com>
2022-01-30 15:21:21 -0500

    employer

--- jobs/app.py
+++ jobs/app.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template, g
 import sqlite3
 
-PATH = "db/jobs.sqlite"
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
 
 def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
-        connection = sqlite3.connect(PATH)
-        g._connection = connection 
+        connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
 
@@ -19,31 +18,30 @@ def execute_sql(sql, values=(), commit=False, single=False):
     if commit == True:
         results = connection.commit()
     else:
-        results = cursor.fetchone() if single else cursor.fetchall()       
+        results = cursor.fetchone() if single else cursor.fetchall()
+
     cursor.close()
     return results
 
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
-    if connection != None:
+    if connection is not None:
         connection.close()
 
-@app.route("/job/<job_id>")
+@app.route('/job/<job_id>')
 def job(job_id):
-    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?',[job_id], single=True)
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
     return render_template('job.html', job=job)
 
-@app.route("/employer/<employer_id>")
+@app.route('/employer/<employer_id>')
 def employer(employer_id):
-    employer = execute_sql( 'SELECT * FROM employer WHERE id=?', [employer_id], single=True)
-    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', employer_id)
-    reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', employer_id)
-    render_template('employer.html', employer=employer, jobs = jobs, reviews = reviews)
-
-
+    employer = execute_sql('SELECT * FROM employer WHERE id=?', [employer_id], single=True)
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
+    reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
+    return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews) 
 @app.route('/')
 @app.route('/jobs')
 def jobs():
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
-    return render_template('index.html', jobs=jobs) 
+    return render_template('index.html', jobs=jobs)
